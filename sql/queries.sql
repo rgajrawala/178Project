55b98d5b06b0937b039e9897a2cf2051a913c83c
philip d cori <pcori@linux60806.dc.engr.scu.edu>
2019-03-13 02:05:04 -0700

    Add remaining queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -30,15 +30,17 @@ begin
 end;
 /
 show errors;
---test: exec finishRepairJob('r3', 12, '2019-03-10 08:00:00')
+--test: exec updateRepairJob('r3', 15, '2019-03-20 08:00:00')
 
---repairjob must have been updated
+--repairjob must have been updated with remaining attributes
 create or replace procedure finishRepairJob(p_repairjob_id in RepairJob.repairjob_id%type) is
 begin
 	delete from RepairJob where repairjob_id = p_repairjob_id;
 end;
 /
 show errors;
+--test: exec finishRepairJob('r3')
+
 
 create or replace procedure createProblem(p_problem_id in Problem.problem_id%type,
 	p_problem_type in Problem.type%type) as
@@ -78,7 +80,7 @@ show errors;
 --test: exec linkPart('r3', 'Tire', 2)
 
 --------------functions for generating customer bill-------------------
---------------use afer updating, before running finishRepairJob--------
+--------------use afer running finishRepairJob-------------------------
 
 create or replace function getCustName(p_repairjob_id in RepairJob.repairjob_id%type)
 return Meteor_Customer.cust_name%type
@@ -87,13 +89,13 @@ is
 	tmp_contact_info Meteor_Customer.contact_info%type;
 begin
 	select contact_info into tmp_contact_info from Car
-	where license_number = (select license_number from RepairJob where repairjob_id = p_repairjob_id);
+	where license_number = (select license_number from Finished_RepairJob where repairjob_id = p_repairjob_id);
 	select cust_name into res from Meteor_Customer where contact_info = tmp_contact_info;
 	return res;
 end;
 /
 show errors;
---test: select getCustName('r3') from dual;
+--test: select getCustName('r1') from dual;
 
 create or replace function getContactInfo(p_repairjob_id in RepairJob.repairjob_id%type)
 return Meteor_Customer.contact_info%type
@@ -101,12 +103,12 @@ is
 	res Meteor_Customer.contact_info%type;
 begin
 
-	select contact_info into res from Car where license_number = (select license_number from RepairJob where repairjob_id = p_repairjob_id);
+	select contact_info into res from Car where license_number = (select license_number from Finished_RepairJob where repairjob_id = p_repairjob_id);
 	return res;
 end;
 /
 show errors;
---test: select getContactInfo('r3') from dual;
+--test: select getContactInfo('r1') from dual;
 
 create or replace function getAddress(p_repairjob_id in RepairJob.repairjob_id%type)
 return Meteor_Customer.cust_address%type
@@ -115,49 +117,49 @@ is
 	tmp_contact_info Meteor_Customer.contact_info%type;
 begin
 	select contact_info into tmp_contact_info from Car
-	where license_number = (select license_number from RepairJob where repairjob_id = p_repairjob_id);
+	where license_number = (select license_number from Finished_RepairJob where repairjob_id = p_repairjob_id);
 	select cust_address into res from Meteor_Customer where contact_info = tmp_contact_info;
 	return res;
 end;
 /
 show errors;
---test: select getAddress('r3') from dual;
+--test: select getAddress('r1') from dual;
 
 create or replace function getModel(p_repairjob_id in RepairJob.repairjob_id%type)
 return Car.model%type
 is
 	res Car.model%type;
 begin
-	select model into res from Car where license_number = (select license_number from RepairJob where repairjob_id = p_repairjob_id);
+	select model into res from Car where license_number = (select license_number from Finished_RepairJob where repairjob_id = p_repairjob_id);
 	return res;
 end;
 /
 show errors;
---test: select getModel('r3') from dual;
+--test: select getModel('r1') from dual;
 
 create or replace function getTimeIn(p_repairjob_id in RepairJob.repairjob_id%type)
 return RepairJob.time_in%type
 is
 	res RepairJob.time_in%type;
 begin
-	select time_in into res from RepairJob where repairjob_id = p_repairjob_id;
+	select time_in into res from Finished_RepairJob where repairjob_id = p_repairjob_id;
 	return res;
 end;
 /
 show errors;
---test: select getTimeIn('r3') from dual;
+--test: select getTimeIn('r1') from dual;
 
 create or replace function getTimeOut(p_repairjob_id in RepairJob.repairjob_id%type)
 return RepairJob.time_out%type
 is
 	res RepairJob.time_out%type;
 begin
-	select time_out into res from RepairJob where repairjob_id = p_repairjob_id;
+	select time_out into res from Finished_RepairJob where repairjob_id = p_repairjob_id;
 	return res;
 end;
 /
 show errors;
---test: select getTimeOut('r3') from dual;
+--test: select getTimeOut('r1') from dual;
 
 create or replace function getServiceCharge(p_repairjob_id in RepairJob.repairjob_id%type)
 return NUMBER 
@@ -167,29 +169,32 @@ is
 	labor_hrs RepairJob.labor_hours%type;
 begin
 	select hourly_pay_rate into hrly_rate from Mechanic
-		where employee_id = (select employee_id from RepairJob where repairjob_id = p_repairjob_id);
-	select labor_hours into labor_hrs from RepairJob where repairjob_id = p_repairjob_id;
+		where employee_id = (select employee_id from Finished_RepairJob where repairjob_id = p_repairjob_id);
+	select labor_hours into labor_hrs from Finished_RepairJob where repairjob_id = p_repairjob_id;
 	res := hrly_rate * labor_hrs;
 	return res;
 end;
 /
 show errors;
---test: exec updateRepairJob('r3', 10, '2019-03-20 08:00:00')
---select getServiceCharge('r3') from dual;
+--test: select getServiceCharge('r1') from dual;
 
-create or replace function GetTotalCost(p_repairjob_id in RepairJob.repairjob_id%type)
+--only use for finished repairjobs
+create or replace function getTotalCost(p_repairjob_id in RepairJob.repairjob_id%type)
 return NUMBER
 is
 	cost_of_parts Part.cost%type;
 	cost_of_labor NUMBER;
 begin
-	select sum(cost) into cost_of_parts from (Uses NATURAL JOIN Part) where repairjob_id = p_repairjob_id;
+	select sum(cost) into cost_of_parts from (Used NATURAL JOIN Part) where repairjob_id = p_repairjob_id;
+	if cost_of_parts is null then
+		cost_of_parts := 0;
+	end if;
 	cost_of_labor := getServiceCharge(p_repairjob_id);
 	return cost_of_labor + cost_of_parts;
 end;
 /
 show errors;
---test: select gettotalcost('r4') from dual;
+--test: select gettotalcost('r1') from dual;
 
 
 ------------queries to be executed in PHP file for customer bill------------------
@@ -198,8 +203,67 @@ show errors;
 -- COLUMN cost FORMAT $99999.99
 -- select part_name, cost from (Uses NATURAL JOIN Part) where repairjob_id = p_repairjob_id;
 
---get problems
+-- get problems
 -- select problem_id, type from (Fixes NATURAL JOIN Problem) where repairjob_id = 'r3';
 
--- --get problems (if above function doesn't work)
--- select problem_id, type from (Fixes NATURAL JOIN Problem) where repairjob_id = p_repairjob_id;
+------------query for 1.3----------------------------------------------------------
+
+-- get repair jobs between two dates
+-- select * from Finished_RepairJob where time_out > to_timestamp('10-JAN-2019 08:00:00') and time_out < to_timestamp('20-MAR-2019 08:00:00')
+
+
+create or replace function getMechanicWithMostHours
+return Mechanic.employee_id%type
+as
+res Mechanic.employee_id%type;
+begin
+	select employee_id into res from 
+	(select employee_id, sum(labor_hours) as sum from Finished_RepairJob group by employee_id order by sum desc) where rownum = 1;
+	return res;
+end;
+/
+show errors;
+
+create or replace function getMechanicWithLeastHours
+return Mechanic.employee_id%type
+as
+res Mechanic.employee_id%type;
+begin
+	select employee_id into res from 
+	(select employee_id, sum(labor_hours) as sum from Finished_RepairJob group by employee_id order by sum asc) where rownum = 1;
+	return res;
+end;
+/
+show errors;
+
+create or replace function getAverageHoursWorked
+return NUMBER
+as
+res NUMBER;
+begin
+	select avg(sum) into res from (select sum(labor_hours) as sum from Finished_RepairJob group by employee_id order by sum asc);
+	return res;
+end;
+/
+show errors;
+
+create or replace function getTotalRevenue(p_start RepairJob.time_out%type, p_finish RepairJob.time_out%type)
+return NUMBER
+as
+res NUMBER;
+Cursor repairjob_cursor is
+	select * from Finished_RepairJob;
+l_repairjob repairjob_cursor%rowtype;
+begin
+	res := 0;
+	for l_repairjob in repairjob_cursor
+	loop
+		if l_repairjob.time_out > p_start and l_repairjob.time_out < p_finish then
+			res := res + getTotalCost(l_repairjob.repairjob_id);
+		end if;
+	end loop;
+	return res;
+end;
+/
+show errors;
+--test: select gettotalrevenue('2019-02-01 08:00:00','2019-03-27 08:00:00') from dual;
